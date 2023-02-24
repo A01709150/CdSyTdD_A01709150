edb0c41164f6e66240cccd6c4552b290b6c271c9
Diego Perdomo Salcedo <diegoperdomosalcedo@Diegos-MacBook-Air-2.local>
2023-02-24 17:45:16 -0600

    Ultima problema terminado

--- Laboratoria 8/app.js
+++ Laboratoria 8/app.js
@@ -1,13 +1,13 @@
 //Promedio de un arreglo
 function arregloProm(arreglo) {
-    var i = 0, sum = 0, ArrayLen = arreglo.length;
+    let i = 0, sum = 0, ArrayLen = arreglo.length;
     while (i < ArrayLen) {
         sum= sum + arreglo[i++];
 }
     return sum / ArrayLen;
 }
-var arreglo= [1, 2, 3];
-var a = arregloProm(arreglo);
+const arreglo = [1, 2, 3];
+let a = arregloProm(arreglo);
 console.log(a)
 
 //Recibe string y lo imprime en archivo de texto
@@ -16,22 +16,22 @@ function stringText(string){
     filesystem.writeFileSync('NICE.txt', string) 
     console.log("Se creo el archivo NICE.txt")
 }
-var string = "LO LOGRE";
+let string = "LO LOGRE";
 stringText(string);
 
 //Funcion que calcula el promedio de un arregle y lo imprime en un archivo de texto
-function promText(arreglo){
-    var i = 0, sum = 0, ArrayLen = arreglo.length;
+function promText(arreglo1){
+    let i = 0, sum = 0, ArrayLen = arreglo1.length;
     while (i < ArrayLen) {
         sum = sum + arreglo[i++];
 }
-    var avg = sum / ArrayLen;
-    var avgText = avg.toString();
+    let avg = sum / ArrayLen;
+    let avgText = avg.toString();
     const filesystem = require('fs');
     filesystem.writeFileSync('GOOD.txt', avgText) 
     console.log("Se creo el archivo GOOD.txt")
 }
-var arreglo = [1, 2, 3];
+let arreglo1 = [1, 2, 3];
 promText(arreglo);
 
 //aplicación web que al enviar una petición al servidor, devuelva una de las páginas que creaste anteriormente en tus laboratorios.
@@ -41,12 +41,14 @@ const http = require('http');
 const server = http.createServer( (request, response) => {    
     console.log(request.url);
     response.setHeader('Content-Type', 'text/html');
-    response.write("<h1>Chilaquiles</h1>");
-    response.write("Hola desde node!");
+    const fs = require("fs");
+    const buffer = fs.readFileSync("/Users/diegoperdomosalcedo/CdSyTdD_A01709150/Laboratorio 1/index.html");
+    const fileContent = buffer.toString();
+    response.write(fileContent);
     response.end();
 });
 
-server.listen(3015);
+server.listen(3024);
 
 
 
