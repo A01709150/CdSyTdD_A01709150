a45106732f5bb486e05fd40cb54f5db59d7dffca
Diego Perdomo Salcedo <diegoperdomosalcedo@Diegos-MacBook-Air-2.local>
2023-03-07 10:58:59 -0600

    Lab 12 terminado

--- Laboratorio 11/routes/juegos.routes.js
+++ Laboratorio 11/routes/juegos.routes.js
@@ -2,36 +2,41 @@ const express = require('express');
 
 const router = express.Router();
 
-const razas = [
+const juegos = [
     { 
-        raza: "Zelda Breath of the Wild", 
+        juego: "Zelda Breath of the Wild", 
         imagen: "https://assets-prd.ignimgs.com/2022/06/14/zelda-breath-of-the-wild-1655249167687.jpg", 
-        descripcion: "Explora y descubre un mundo lleno de aventuras en The Legend of Zelda: Breath of the Wild, una nueva saga que rompe los esquemas de la aclamada serie."
+        descripcion: "Explora y descubre un mundo lleno de aventuras en The Legend of Zelda: Breath of the Wild, una nueva saga que rompe los esquemas de la aclamada serie.",
+        precio: "60 dolares / 1,200 pesos mexicanos"
     }, 
     {
-        raza: "Pokemon Scarlet",
+        juego: "Pokemon Scarlet",
         imagen: "https://sm.ign.com/ign_latam/cover/p/pokemon-sc/pokemon-scarlet_cd8u.jpg", 
-        descripcion: "Explora Paldea en la busqueda de encontrar que son los pokemones tan raros que estan por esta nueva región. "
+        descripcion: "Explora Paldea en la busqueda de encontrar que son los pokemones tan raros que estan por esta nueva región. ",
+        precio: "60 dolares / 1,200 pesos mexicanos"
     }, 
     { 
-        raza: "Pokemon Violet",
+        juego: "Pokemon Violet",
         imagen: "https://www.latercera.com/resizer/JiTuUlBOY2lIe31moj1lzBZYQaA=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/JSI7T5BENZBTBOARJPYJFHCMIE.jpg", 
-        descripcion: "Explora Paldea en la busqueda de encontrar que son los pokemones tan raros que estan por esta nueva región. "
+        descripcion: "Explora Paldea en la busqueda de encontrar que son los pokemones tan raros que estan por esta nueva región. ",
+        precio: "60 dolares / 1,200 pesos mexicanos"
     },
     { 
-        raza: "Luigi's Mansion",
-        imagen: "https://bulma.io/images/placeholders/1280x960.png", 
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris."
+        juego: "Luigi's Mansion",
+        imagen: "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_LuigisMansion3_image1280w.jpg", 
+        descripcion: "Luigi ha sido invitado al hotel El Gran Descanso Real, ¡pero cuando Mario y sus amigos desaparecen, Luigi tendrá que superar sus miedos para salvarlos.",
+        precio: "60 dolares / 1,200 pesos mexicanos"
     },
     { 
-        raza: "Mario Odyssey",
+        juego: "Mario Odyssey",
         imagen: "https://http2.mlstatic.com/D_NQ_NP_951095-MLA29062841364_122018-O.jpg", 
-        descripcion: "Acompaña a Mario en una aventura enorme en 3D y rescata a la princesa Peach de su boda con Bowser."
+        descripcion: "Acompaña a Mario en una aventura enorme en 3D y rescata a la princesa Peach de su boda con Bowser.",
+        precio: "60 dolares / 1,200 pesos mexicanos"
     } 
 ];
 
 router.get('/', (request, response, next) => {
-    response.render('lista', {razas: razas});
+    response.render('lista', {juegos: juegos});
 });
 
 module.exports = router;
