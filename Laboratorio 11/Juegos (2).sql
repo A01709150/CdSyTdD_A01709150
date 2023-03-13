0abb4e3722f989ecc835145449e76f32889ebf4d
Diego Perdomo Salcedo <diegoperdomosalcedo@Diegos-MacBook-Air-2.local>
2023-03-13 12:08:42 -0600

    Lab 15

---
+++ Laboratorio 11/Juegos (2).sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 13, 2023 at 05:05 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Juegos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Juegos`
+--
+
+CREATE TABLE `Juegos` (
+  `id_juego` int(11) NOT NULL,
+  `juego` varchar(100) NOT NULL,
+  `imagen` varchar(500) NOT NULL,
+  `descripción` varchar(500) NOT NULL,
+  `precio` float(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Juegos`
+--
+ALTER TABLE `Juegos`
+  ADD PRIMARY KEY (`id_juego`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
